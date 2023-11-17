2b0dadb2c9b0f62f6d6b3138fe38ac69683ea713
Vaya Line <134380865+aqcchi@users.noreply.github.com>
2023-11-17 23:24:36 +0200

    Add files via upload

---
+++ Python-OOP-Exercises/Polymorphism and Abstraction - Exercise/06_formula_1_manager/formula_teams/mercedes_team.py
@@ -0,0 +1,12 @@
+from project.formula_teams.formula_team import FormulaTeam
+
+
+class MercedesTeam(FormulaTeam):
+
+    @staticmethod
+    def get_team_data():
+        expenses = 200000
+        sponsors = {"Patronas": {1: 1000000, 3: 500000},
+                    "TeamViewer": {5: 100000, 7: 50000}}
+
+        return expenses, sponsors
