e6c11383c7ab4aa5e824cd0ca25306c29391501b
Anelia Varadinova <134380865+aqcchi@users.noreply.github.com>
2024-03-20 16:36:55 +0200

    Add files via upload

---
+++ JS Front-End/DOM Introduction - Lab/04.Show More/ShowMore.js
@@ -0,0 +1,9 @@
+function showText() {
+    
+    const moreButoon = document.getElementById('more');
+    const elementToAppear = document.getElementById('text');
+
+    moreButoon.style.display = 'none';
+    elementToAppear.style.display = 'inline';
+    
+}
