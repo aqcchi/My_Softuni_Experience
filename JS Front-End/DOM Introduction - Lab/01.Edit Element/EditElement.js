efca092bd906b261f9172138c10d239ad2dc423b
Anelia Varadinova <134380865+aqcchi@users.noreply.github.com>
2024-03-20 15:20:13 +0200

    Add files via upload

---
+++ JS Front-End/DOM Introduction - Lab/01.Edit Element/EditElement.js
@@ -0,0 +1,7 @@
+function editElement(element, match, replacer) { 
+    
+    while (element.textContent.includes(match)) {
+        element.textContent = element.textContent.replace(match, replacer)
+    }
+
+}
