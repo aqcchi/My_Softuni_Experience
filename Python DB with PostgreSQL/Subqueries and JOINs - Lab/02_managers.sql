b0b14033c2393ac62b77f478820aa5ce693a6608
Anelia Varadinova <134380865+aqcchi@users.noreply.github.com>
2024-06-04 14:08:51 +0300

    Create 02_managers.sql

---
+++ Python DB with PostgreSQL/Subqueries and JOINs - Lab/02_managers.sql
@@ -0,0 +1,16 @@
+SELECT 
+	e.employee_id, 
+	CONCAT(
+	e.first_name,
+	' ',
+	e.last_name
+	) AS full_name, 
+	d.department_id,
+	d.name AS department_name
+FROM 
+	employees as e
+		JOIN departments as d
+			ON e.employee_id = d.manager_id
+ORDER BY 
+	e.employee_id
+LIMIT 5
