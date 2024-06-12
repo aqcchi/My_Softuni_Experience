7ac6ddc44fab029d5dd79d55ff851c3dfebd2f7a
Anelia Varadinova <134380865+aqcchi@users.noreply.github.com>
2024-06-12 14:20:29 +0300

    Create 2.4_delete.sql

---
+++ Python DB with PostgreSQL/Exam Preparation 1/2.4_delete.sql
@@ -0,0 +1,16 @@
+DELETE FROM 
+	volunteers
+WHERE 
+	department_id = (
+		SELECT 
+			id
+		FROM
+			volunteers_departments
+		WHERE
+			department_name = 'Education program assistant'
+	);
+
+DELETE FROM
+	volunteers_departments 
+WHERE
+	department_name = 'Education program assistant';
